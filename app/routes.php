dd214cca6f67b29abdf8a620816023ff7320e221
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-20 13:21:40 +0600

    saving the url to the database and generate short URL

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,49 @@ Route::get('/', function()
 {
 	return View::make('form');
 });
+Route::post('/', function(){
+    // defining validation rules comes first
+    $data = Input::all();
+    //dd($data['link']);
+    $rules = array(
+        'link' => 'required|url'
+    );
+    // then we run the form validation
+    $validation = Validator::make($data,$rules);
+
+    //If validation fails, we return to the main page with an error info
+    if($validation->fails()){
+        return Redirect::to('/')
+            ->withInput()
+            ->withErrors($validation);
+    }
+    else {
+        //dd("Here it comes");
+        //Now let's check if we already have the link in our database. If so, we get the first result
+        $link = Link::where('url', '=', $data['link'])->first();
+        //If we have the URL saved in our database already, we provide that information back to view.
+        if($link){
+            return Redirect::to('/')
+                ->withInput()
+                ->with('link', $link->hash);
+        }
+        //Else we create a new unique URL
+        else {
+            //First we create a new unique Hash
+            do{
+                $newHash = Str::random(6);
+            }
+            while(Link::where('hash', '=', $newHash)->count() > 0);
+            //Now we create a new database record
+            Link::create(array(
+                'url' => $data['link'],
+                'hash' => $newHash
+            ));
+            //And then we return the new shortened URL info to our action
+            return Redirect::to('/')
+                ->withInput()
+                ->with('link', $newHash);
+        }
+
+    }
+});
--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -8,8 +8,20 @@
 <body>
   <div id="container">
       <h2>URL Shortener</h2>
+
+      @if(Session::has('errors'))
+      <h3 class="error">{{ $errors->first('link') }}</h3>
+      @endif
+
+      @if(Session::has('link'))
+      <h3 class="success">
+      {{ HTML::link(Session::get('link'), 'Click here for your shorten URL') }}
+      </h3>
+      @endif
+
       {{ Form::open(array('url'=>'/','method' => 'post')) }}
         {{ Form::text('link', Input::old('link'), array('placeholder'=>'Insert your URL here and press enter!')) }}
+        {{ Form::submit('Submit') }}
       {{ Form::close() }}
   </div>
 </body>
